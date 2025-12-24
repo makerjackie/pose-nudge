fd53106fe3acae4fa4d665519e95626393eff0a0
yusufyorunc <yusufyorunc65@gmail.com>
2025-12-24 23:00:43 +0300

    feat: Add Turkish (tr) language support

--- src/i18n.ts
+++ src/i18n.ts
@@ -7,6 +7,7 @@ import translationEN from './locales/en/translation.json';
 import translationKO from './locales/ko/translation.json';
 import translationJA from './locales/ja/translation.json';
 import translationZH from './locales/zh/translation.json';
+import translationTR from './locales/tr/translation.json';
 
 const resources = {
   en: {
@@ -20,6 +21,9 @@ const resources = {
   },
   zh: {
     translation: translationZH,
+  },
+  tr: {
+    translation: translationTR,
   },
 };
 
